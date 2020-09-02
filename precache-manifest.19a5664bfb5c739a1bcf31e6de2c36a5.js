9f21ada6b87877f0e3b63a17882ba0faf4c9e5f2
WineXerox <tanapol.ta.61@ubu.ac.th>
2020-09-02 10:48:45 +0700

    Updates

---
+++ precache-manifest.19a5664bfb5c739a1bcf31e6de2c36a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57a5f9d531a60112e08b7f900b1e4513",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "691dae3e823191b14df6",
+    "url": "/tic-tac-toe/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "642f11ce68d236595a80",
+    "url": "/tic-tac-toe/static/js/2.a2c56392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.a2c56392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "691dae3e823191b14df6",
+    "url": "/tic-tac-toe/static/js/main.93cc1a03.chunk.js"
+  },
+  {
+    "revision": "4ad95f08d6b4d56d4069",
+    "url": "/tic-tac-toe/static/js/runtime-main.fa7aabe4.js"
+  }
+]);
